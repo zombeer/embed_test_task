04ea518362add3a83ff7c239964b7d15e0702ad9
Michael Savchenko <zombeer@gmail.com>
2022-09-04 00:01:42 +0200

    Let's do it using schema-first approach as it's seems to be most futureproof to me.

---
+++ schemas.py
@@ -0,0 +1,36 @@
+from datetime import date, datetime
+
+from pydantic import BaseModel
+
+
+class OrmModel(BaseModel):
+    class Config:
+        orm_mode = True
+
+
+class Post(OrmModel):
+    id: int | None = None
+    title: str
+    text: str
+    timestamp: datetime
+    is_new: bool = False
+
+
+class User(OrmModel):
+    id: int | None = None
+    name: str
+    password: str | None
+    country: str | None = None
+    city: str | None = None
+    birthDate: date | None = None
+    interests: list[str] = []
+    bio: str | None = None
+    posts: list[Post] = []
+    subscribers_count: int = 0
+    subscriptions_count: int = 0
+    last_login: datetime | None = None
+
+
+class GenericApiResponse(BaseModel):
+    code: int
+    message: str
