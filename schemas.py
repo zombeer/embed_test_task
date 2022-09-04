24988a14224135948d262f575c7eba9c7a08a8e4
Michael Savchenko <zombeer@gmail.com>
2022-09-04 16:02:58 +0200

    Schemas updated. Added validators for username and password

--- schemas.py
+++ schemas.py
@@ -1,6 +1,8 @@
+import re
 from datetime import date, datetime
 
-from pydantic import BaseModel
+from fastapi import Body
+from pydantic import BaseModel, validator
 
 
 class OrmModel(BaseModel):
@@ -9,7 +11,7 @@ class OrmModel(BaseModel):
 
 
 class Post(OrmModel):
-    id: int | None = None
+    id: int
     title: str
     text: str
     timestamp: datetime
@@ -17,24 +19,47 @@ class Post(OrmModel):
 
 
 class User(OrmModel):
-    id: int | None = None
     name: str
     password: str | None
     country: str | None = None
     city: str | None = None
-    birthDate: date | None = None
+    birthdate: date | None = None
     interests: list[str] = []
     bio: str = ""
     posts: list[Post] = []
     subscriptions: list[str] = []
     subscribers_count: int = 0
     subscriptions_count: int = 0
-    last_login: datetime | None = None
+    last_activity: datetime | None = None
 
 
 class GenericApiResponse(BaseModel):
     code: int
     message: str
+
+
+class UserPasswordPayload(BaseModel):
+    username: str = Body(..., title="Username of new user")
+    password: str = Body(..., title="Password for the new user")
+
+    @validator("username")
+    def validate_username(cls, v):
+        assert v.isalnum(), "Username must contain only letters or numbers"
+        assert len(v) < 15, "Username length must be less than 15 symbols"
+        assert len(v) > 3, "Username length must be more than 3 symbols"
+        return v
+
+    @validator("password")
+    def validate_password(cls, v):
+        assert len(v) > 8, "Password should be longer than 8 symbols"
+        assert bool(re.search(r"\d", v)), "Password must contain at least one digit"
+        assert bool(
+            re.search(r"[A-Z]", v)
+        ), "Password must contain at least one uppercase letter"
+        assert bool(
+            re.search(r"[!@#$%^&*()_]", v)
+        ), "Password must contain at least one symbol out of !@#$%^&*()_"
+        return v
 
 
 class Token(BaseModel):
