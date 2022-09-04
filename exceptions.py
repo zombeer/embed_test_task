49a4004481b61a43e06515eaca0ccb089c81bb5e
Michael Savchenko <zombeer@gmail.com>
2022-09-04 22:06:39 +0200

    Add some comments to exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,15 +1,17 @@
 from fastapi import HTTPException, status
 
+# Exception to handle auth errors
 not_authorized_exception = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Incorrect username or password",
 )
 
+# Exeption to handle duplicate username error
 user_exists_exception = HTTPException(
     status_code=status.HTTP_409_CONFLICT, detail="Username already exists"
 )
 
-
+# Exception to handle user not found error
 user_not_found_exception = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
 )
