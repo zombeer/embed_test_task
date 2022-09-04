43e06fe3574d304bd158cedb8163517bcfed7ab2
Michael Savchenko <zombeer@gmail.com>
2022-09-04 20:32:41 +0200

    exceptions.py update

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,15 @@
 from fastapi import HTTPException, status
 
-credentials_exception = HTTPException(
+not_authorized_exception = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Incorrect username or password",
 )
 
 user_exists_exception = HTTPException(
-    status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Username already exists"
+    status_code=status.HTTP_409_CONFLICT, detail="Username already exists"
+)
+
+
+user_not_found_exception = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
 )
