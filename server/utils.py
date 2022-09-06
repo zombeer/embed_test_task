04c075389792150ca0b2a0557ba2050d6c61ff28
Michael Savchenko <zombeer@gmail.com>
2022-09-06 11:57:20 +0200

    server.py refactored into package.

--- server/utils.py
+++ server/utils.py
@@ -1,14 +1,35 @@
-from jose import jwt
-from passlib.context import CryptContext
-
 from exceptions import not_authorized_exception, user_not_found_exception
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
 from models import User
+from passlib.context import CryptContext
 
 SECRET_KEY = "some_super_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
+    """
+    Auth helper. Gets instance of current User using token payload or raise corresponding exception.
+    """
+    try:
+        payload = decode_jwt(token)
+        username = payload.get("sub")
+        if username is None:
+            raise user_not_found_exception
+    except JWTError:
+        raise not_authorized_exception
+    user = User.get_or_none(name=username)
+    if user is None:
+        raise not_authorized_exception
+    # Update last user activity every time we see a token from him/her
+    user.bump()
+    return user
 
 
 def authenticate_user(username: str, password: str) -> User | None:
