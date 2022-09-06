5adb1311a19d56894d1b732db7461aba21af4176
Michael Savchenko <zombeer@gmail.com>
2022-09-06 15:46:27 +0200

    some improvements to run code locally.

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 import os
 
 # Default database(sqlite3)
-DB_FALLBACK_URI = "sqlite:////database/embed_xyz_api_fallback.db"
+
+DB_FALLBACK_URI = "sqlite:////database/embed_xyz_api.db"
 
 # Target database, use format as mentioned here:
 # https://docs.peewee-orm.com/en/latest/peewee/playhouse.html#connect
