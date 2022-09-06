1c19a3e05c5fc00582025490fbec98e496930ab7
Michael Savchenko <zombeer@gmail.com>
2022-09-06 13:14:01 +0200

    fallback database name updated.

--- models/db.py
+++ models/db.py
@@ -4,7 +4,7 @@ from playhouse.db_url import connect
 
 
 def get_db():
-    db_url = os.getenv("DB_URL") or "sqlite:///default.db"
+    db_url = os.getenv("DB_URL") or "sqlite:///embed_xyz_api.db"
     return connect(db_url)
 
 
