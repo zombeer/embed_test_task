6ad018d8fe0f13496b6ba24814e724e34a683f18
Michael Savchenko <zombeer@gmail.com>
2023-06-06 14:18:42 +0200

    return type fix

--- src/models/db.py
+++ src/models/db.py
@@ -3,7 +3,7 @@ from playhouse.db_url import connect
 from config import DB_URI
 
 
-def get_db() -> connect:
+def get_db():  # noqa: ANN201
     """Returns a database connection."""
     return connect(DB_URI)
 
