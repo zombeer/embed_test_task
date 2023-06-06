7449cf103397d1bd2150f4468ca603ca5319cca7
Michael Savchenko <zombeer@gmail.com>
2023-06-06 13:44:10 +0200

    codestyle fix

--- src/server/utils.py
+++ src/server/utils.py
@@ -6,7 +6,7 @@ from passlib.context import CryptContext
 from exceptions import not_authorized_exception, user_not_found_exception
 from models import User
 
-SECRET_KEY = "some_super_secret_key"
+SECRET_KEY = "some_super_secret_key"  # noqa: S105
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -19,10 +19,12 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     try:
         payload = decode_jwt(token)
         username = payload.get("sub")
-        if username is None:
-            raise user_not_found_exception
     except JWTError:
         raise not_authorized_exception
+
+    if not username:
+        raise user_not_found_exception
+
     user = User.get_or_none(name=username)
     if user is None:
         raise not_authorized_exception
