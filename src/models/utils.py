173ea2d14783f474742320cf2538b4293578ab95
Michael Savchenko <zombeer@gmail.com>
2023-06-06 13:51:29 +0200

    some cleanup

--- src/models/utils.py
+++ src/models/utils.py
@@ -4,14 +4,11 @@ from models import Post, Subscription, User, db
 
 
 def add_user(username: str, password: str) -> User | None:
-    """
-    Creates and returns new User() or None if user exists.
-    """
-    user, created = User.get_or_create(name=username, defaults=dict(password=password))
+    """Creates and returns new User() or None if user exists."""
+    user, created = User.get_or_create(name=username, defaults={"password": password})
     if created:
         return user
-    else:
-        return None
+    return None
 
 
 def post_filter_query_builder(
@@ -20,10 +17,10 @@ def post_filter_query_builder(
     start: date | None = None,
     end: date | None = None,
 ):
-    """
-    Builds filter query to be applied to list posts query.
+    """Builds filter query to be applied to list posts query.
+
     User can search his/her posts by the title name via a simple substring match.
-    User can search his/her posts by date published via start date and end date filters
+    User can search his/her posts by date published via start date and end date filters.
     """
     if keyword:
         query = query.where(Post.title.contains(keyword))
@@ -34,10 +31,8 @@ def post_filter_query_builder(
     return query
 
 
-def get_top_users(limit: int = 20):
-    """
-    Custom rating implemented as sum of user subscribers count and posts count.
-    """
+def get_top_users(limit: int = 20) -> list[User]:
+    """Custom rating implemented as sum of user subscribers count and posts count."""
     raw_query = f"""
         SELECT u.*, COUNT(scores.x) AS score
         FROM users u
@@ -53,8 +48,6 @@ def get_top_users(limit: int = 20):
     return User.raw(raw_query)
 
 
-def create_tables():
-    """
-    Helper to initialize tables
-    """
+def create_tables() -> None:
+    """Helper to initialize tables."""
     db.create_tables([User, Post, Subscription])
